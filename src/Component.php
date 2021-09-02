ff11541bb7a07712126c059ec080413fdeb9297e
Vadim Sukhoykin <vadim.sukhoykin@gmail.com>
2021-09-02 21:39:05 +0300

    Base Component-Composite Structure

--- src/Component.php
+++ src/Component.php
@@ -2,35 +2,9 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace Sukhoykin\App;
 
-use Psr\Container\ContainerInterface;
-use Exception;
-
-class Component
+interface Component
 {
-    private $container;
-
-    public function __construct(ContainerInterface $container)
-    {
-        $this->container = $container;
-    }
-
-    public function container(): ContainerInterface
-    {
-        return $this->container;
-    }
-
-    public function instantiate($class)
-    {
-        if (!class_exists($class)) {
-            throw new Exception('Component class ' . $class . ' not exists');
-        }
-
-        try {
-            return new $class($this->container);
-        } catch (Exception $e) {
-            throw new Exception('Class ' . $class . ' must be ' . Component::class . ' instance', 0, $e);
-        }
-    }
+    function invoke(Composite $root);
 }
