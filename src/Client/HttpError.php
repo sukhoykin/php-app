26f48c4be95d1fc248f41ee242fee190406daee3
Vadim Sukhoykin <vadim.sukhoykin@gmail.com>
2021-11-18 17:32:22 +0300

    HttpError import

---
+++ src/Client/HttpError.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sukhoykin\App\Client;
+
+use Exception;
+
+class HttpError extends Exception {
+}
--- src/Client/RestClient.php
+++ src/Client/RestClient.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Sukhoykin\App\Client;
 
 use Exception;
-use App\Error\HttpError;
 
 class RestClient extends HttpClient
 {
